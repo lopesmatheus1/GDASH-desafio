c66d51841ab101c2edf9ca43b5f1297adcf09609
lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-11-28 16:20:27 -0300

    feat: enhance CreateWeatherDto with validation rules for all properties

--- nest-api/src/weather/dto/create-weather.dto.ts
+++ nest-api/src/weather/dto/create-weather.dto.ts
@@ -1,8 +1,37 @@
+import { Type } from 'class-transformer'
+import { IsDefined, IsNumber, IsString, Min, Max } from 'class-validator'
+
 export class CreateWeatherDto {
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'temperature must be a number' })
   temperature: number
+
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'humidity must be a number' })
+  @Min(0)
+  @Max(100)
   humidity: number
+
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'windSpeed must be a number' })
   windSpeed: number
+
+  @IsDefined()
+  @IsString({ message: 'weatherCondition must be a string' })
   weatherCondition: string
+
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'weatherCode must be an integer' })
   weatherCode: number
+
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber({}, { message: 'precipitationProbability must be an integer' })
+  @Min(0)
+  @Max(100)
   precipitationProbability: number
 }
