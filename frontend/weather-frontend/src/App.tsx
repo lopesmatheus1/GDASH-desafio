7e24817f3465d355ffcfc3402987001ba9f7b179
lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-11-30 13:16:32 -0300

    chore: initialize react project with vite

---
+++ frontend/weather-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="read-the-docs">
+        Hello World! This is the Weather Frontend Application.
+      </p>
+    </>
+  );
+}
+
+export default App;
