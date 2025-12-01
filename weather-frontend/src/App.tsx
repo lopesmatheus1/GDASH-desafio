842da04093ecd4453791dca54deb3efdb0763c67
lopesmatheus1 <lopesmatheus1@hotmail.com>
2025-11-30 23:54:05 -0300

    feat: restructure App component and add DashboardPage

--- weather-frontend/src/App.tsx
+++ weather-frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import { Button } from './components/ui/button'
+import { BrowserRouter, Route, Routes } from 'react-router'
+
+import AuthenticationPage from './pages/authentication'
+import DashboardPage from './pages/dashboard'
 
 function App() {
   return (
-    <>
-      <div>
-        <Button>teste</Button>
-        <p>Hello world</p>
+    <BrowserRouter>
+      <div className="min-h-screen bg-slate-950 text-slate-100 font-sans">
+        <Routes>
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/authentication" element={<AuthenticationPage />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
